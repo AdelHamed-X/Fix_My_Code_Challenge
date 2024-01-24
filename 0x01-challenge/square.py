aa4568f7263badcd2e9bc709245944d148d62be1
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-01-24 09:24:26 +0200

    Adding Task 0

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+""" Class Square Module """
+
 
 class square():
-    
+    """ Class Square """
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """ Instance creation """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return (self.width * 2 ) * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String Representation of an instance """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
